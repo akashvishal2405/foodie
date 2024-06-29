9a9b59133b07faa2079c5e55b26575daf613daac
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-29 11:51:12 +0530

    add new files

---
+++ Components/Icecream.js
@@ -0,0 +1,31 @@
+import Singleflavor from '../Image/icecream/singleflavor.jpg';
+import ThreeFlavor from '../Image/icecream/3flavor.jpg';
+import ThreeFlavorwithFruits from '../Image/icecream/3flavorwithfurit.jpg';
+import DryNuts from '../Image/icecream/drynuts.jpg';
+
+export const Icecream = [
+    {
+        id: 1,
+        image:{Singleflavor},
+        iteamName:"Single Flavor",
+        price: "50"
+    },
+    {
+        id: 2,
+        image:{ThreeFlavor},
+        iteamName:"3 Flavor",
+        price: "90"
+    },
+    {
+        id: 3,
+        image:{ThreeFlavorwithFruits},
+        iteamName:"3 Flavor with Fruits",
+        price: "120"
+    },
+    {
+        id: 4,
+        image:{DryNuts},
+        iteamName:"Dry Nuts",
+        price: "150"
+    }
+]
