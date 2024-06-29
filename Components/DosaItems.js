af3a60504a1ff3228c0336265eee3c637cf25a11
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-29 12:26:55 +0530

    add new files

--- Components/DosaItems.js
+++ Components/DosaItems.js
@@ -1,9 +1,133 @@
-
+import KallDosa from '../Image/dosa/kalldosa.jpg';
+import KallNice from '../Image/dosa/kallnice.jpg';
+import Eggkall from '../Image/dosa/eggkall.jpg';
+import Eggkallnice from '../Image/dosa/eggkallnice.jpg';
+import GeeRost from '../Image/dosa/geerost.jpeg';
+import MasalaDosa from '../Image/dosa/masaladosa.jpg';
+import PodiDosa from '../Image/dosa/podidosa.jpg';
+import OnionDosa from '../Image/dosa/oniondosa.jpg';
+import OnionUthappam from '../Image/dosa/onionuthappam.jpg';
+import Uthappam from '../Image/dosa/uthappam.jpg';
+import SplUthappam from '../Image/dosa/spluthappam.jpg';
+import Chickendosai from '../Image/dosa/chickendosa.jpg';
+import muttondosa from '../Image/dosa/muttondosa.jpg';
+import FishDosai from '../Image/dosa/fishdosa.jpg';
+import Crab from '../Image/dosa/crab.jpg';
+import Plaindosai from '../Image/dosa/plaindosa.jpg';
+import Eggmasaladosai from '../Image/dosa/eggmasaladosa.jpg';
+import Vegmasaladosai from '../Image/dosa/vegmasaladosa.jpg';
 
 
 
 export const DosaItems = [
-    {
-        
-    }
+
+        {
+            id: 1,
+            image:{KallDosa},
+            iteamName:"Kall Dosa",
+            price: "20"
+        },
+        {
+            id: 2,
+            image:{KallNice},
+            iteamName:"Kall Dosa",
+            price: "25"
+        },
+        {
+            id: 3,
+            image:{Eggkall},
+            iteamName:"Egg Kall",
+            price: "35"
+        },
+        {
+            id: 4,
+            image:{Eggkallnice},
+            iteamName:"Egg Kall Nice",
+            price: "45"
+        },
+        {
+            id: 5,
+            image:{GeeRost},
+            iteamName:"Gee Rost",
+            price: "80"
+        },
+        {
+            id: 6,
+            image:{MasalaDosa},
+            iteamName:"Masala Dosa",
+            price: "80"
+        },
+        {
+            id: 7,
+            image:{PodiDosa},
+            iteamName:"Podi Dosa",
+            price: "80"
+        },
+        {
+            id: 8,
+            image:{OnionDosa},
+            iteamName:"Onion Dosa",
+            price: "80"
+        },
+        {
+            id: 9,
+            image:{OnionUthappam},
+            iteamName:"Onion Uthappam",
+            price: "50"
+        },
+        {
+            id: 10,
+            image:{Uthappam},
+            iteamName:"Uthappam",
+            price: "50"
+        },
+        {
+            id: 11,
+            image:{SplUthappam},
+            iteamName:"Special Uthappam",
+            price: "60"
+        },
+        {
+            id: 12,
+            image:{Chickendosai},
+            iteamName:"Chicken Dosai",
+            price: "140"
+        },
+        {
+            id: 13,
+            image:{muttondosa},
+            iteamName:"Mutton Dosai",
+            price: "190"
+        },
+        {
+            id: 14,
+            image:{FishDosai},
+            iteamName:"Fish Dosai",
+            price: "180"
+        },
+        {
+            id: 15,
+            image:{Crab},
+            iteamName:"Crab Omblat Dosai",
+            price: "210"
+        },
+        {
+            id: 16,
+            image:{Plaindosai},
+            iteamName:"Plain Dosai",
+            price: "50"
+        },
+        {
+            id: 17,
+            image:{Eggmasaladosai},
+            iteamName:"Egg Masala Dosai",
+            price: "80"
+        },
+        {
+            id: 18,
+            image:{Vegmasaladosai},
+            iteamName:"Veg Masala Dosai",
+            price: "60"
+        }
+
 ]
