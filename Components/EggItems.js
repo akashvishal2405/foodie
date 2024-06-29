95a1c4fb59b5a8cf9edb3aaf7e50d87336fe981f
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-29 13:01:04 +0530

    add new files

---
+++ Components/EggItems.js
@@ -0,0 +1,73 @@
+import BoiledEgg from '../Image/Eggitems/boiledegg.jpg';
+import Eggmasala from '../Image/Eggitems/eggmasala.jpg';
+import Fullboil from '../Image/Eggitems/fullegg.jpg';
+import Offboil from '../Image/Eggitems/offbouild.jpg';
+import Oneside from '../Image/Eggitems/oneside.jpg';
+import Omelette from '../Image/Eggitems/omelette.jpg';
+import Plain from '../Image/Eggitems/plain.jpg';
+import Podi from '../Image/Eggitems/podi.jpg';
+import Kalakki from '../Image/Eggitems/kalakki.jpeg';
+
+
+export const EggItems = [
+        {
+            id: 1,
+            image:{BoiledEgg},
+            iteamName:"Boiled Egg",
+            price: "20"
+        },
+        {
+            id: 2,
+            image:{Eggmasala},
+            iteamName:"Egg Masala",
+            price: "30"
+        },
+        {
+            id: 3,
+            image:{Fullboil},
+            iteamName:"Full Boil",
+            price: "15"
+        },
+        {
+            id: 4,
+            image:{Offboil},
+            iteamName:"Off Boil",
+            price: "15"
+        },
+        {
+            id: 5,
+            image:{Oneside},
+            iteamName:"One Side",
+            price: "15"
+        },
+        {
+            id: 6,
+            image:{Kalakki},
+            iteamName:"Kalakki",
+            price: "20"
+        },
+        {
+            id: 7,
+            image:{Kalakki},
+            iteamName:"Kazhammu Kalakki",
+            price: "25"
+        },
+        {
+            id: 8,
+            image:{Omelette},
+            iteamName:"Omelette",
+            price: "20"
+        },
+        {
+            id: 9,
+            image:{Plain},
+            iteamName:"Plain Omelette",
+            price: "15"
+        },
+        {
+            id: 10,
+            image:{Podi},
+            iteamName:"Podimass",
+            price: "15"
+        }
+]
