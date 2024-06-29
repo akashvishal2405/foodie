8371768e47ecb7555f130a200409e398ada7fc6c
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-29 15:09:33 +0530

    add new files

---
+++ Components/Juice.js
@@ -0,0 +1,208 @@
+import FreshJucie from '../Image/juice/freshjuice.jpg';
+import kolisoda from '../Image/juice/kolisoda.jpg';
+import Mazza from '../Image/juice/maaza.jpg';
+import water from '../Image/juice/mineralwater.jpg';
+import pulpyorange from '../Image/juice/pulpyorange.jpg';
+import rosemilk from '../Image/juice/rosemilk.jpg';
+import sprite from '../Image/juice/sprite.jpg';
+import Americannuts from '../Image/juice/Americannuts.jpg';
+import blackcurrant from '../Image/juice/blackcurrant.jpg';
+import butterscotch from '../Image/juice/butterscotch.jpg';
+import choclate from '../Image/juice/choclate.jpg';
+import cookiescream from '../Image/juice/cookiescream.jpg';
+import dryfruit from '../Image/juice/dryfruit.jpg';
+import kulfi from '../Image/juice/kulfi.jpg';
+import kulfi from '../Image/juice/kulfi.jpg';
+import red from '../Image/juice/red.jpg';
+import sprite from '../Image/juice/sprite.jpg';
+import seven from '../Image/juice/7up.jpg';
+import badammilk from '../Image/juice/badammilk.jpg';
+import cococola from '../Image/juice/cococola.jpg';
+import cooldrinks from '../Image/juice/cooldrinks.jpg';
+import fanta from '../Image/juice/fanta.jpg';
+
+
+export const juice = [
+
+        {
+            id: 1,
+            image: {FreshJucie},
+            itemName: "Fresh Juice ",
+            price:"55"        
+        },
+        {
+            id: 2,
+            image: {kolisoda},
+            itemName: "Koli Soda ",
+            price:"25"        
+        },
+        {
+            id: 3,
+            image: {Mazza},
+            itemName: "Maaza 600ml",
+            price:"40"        
+        },
+        {
+            id: 4,
+            image: {water},
+            itemName: "Mineral water",
+            price:"20"        
+        },
+        {
+            id: 5,
+            image: {pulpyorange},
+            itemName: "Pulpy Orange",
+            price:"25"        
+        },
+        {
+            id: 6,
+            image: {rosemilk},
+            itemName: "Rose Milk",
+            price:"30"        
+        },
+        {
+            id: 7,
+            image: {sprite},
+            itemName: "Sprite",
+            price:"40"        
+        },
+        {
+            id: 8,
+            image: {water},
+            itemName: "Water btl 300ml",
+            price:"6"        
+        },
+        {
+            id: 9,
+            image: {water},
+            itemName: "Water btl 1lt",
+            price:"20"        
+        },
+        {
+            id: 10,
+            image: {water},
+            itemName: "Water btl 50ml",
+            price:"10"        
+        },
+        {
+            id: 11,
+            image: {Americannuts},
+            itemName: "American Nuts 500ml",
+            price:"199"        
+        },
+        {
+            id: 12,
+            image: {blackcurrant},
+            itemName: "Blackcurrant 500ml",
+            price:"170"        
+        },
+        {
+            id: 13,
+            image: {butterscotch},
+            itemName: "Butterscotch 500ml",
+            price:"160"        
+        },
+        {
+            id: 14,
+            image: {butterscotch},
+            itemName: "Choco cake & Fudge 480ml",
+            price:"299"        
+        },
+        {
+            id: 14,
+            image: {choclate},
+            itemName: "Choco cake & Fudge 480ml",
+            price:"299"        
+        },
+        {
+            id: 15,
+            image: {choclate},
+            itemName: "Chocolate 500ml",
+            price:"160"        
+        },
+        {
+            id: 15,
+            image: {cookiescream},
+            itemName: "Cookies Cream 500ml",
+            price:"199"        
+        },
+        {
+            id: 16,
+            image: {choclate},
+            itemName: "Double Chocolate 500ml",
+            price:"225"        
+        },
+        {
+            id: 17,
+            image: {dryfruit},
+            itemName: "Dry Fruit Magic 900ml",
+            price:"299"        
+        },
+        {
+            id: 18,
+            image: {dryfruit},
+            itemName: "Dry Fruit Magic 900ml",
+            price:"299"        
+        },
+        {
+            id: 19,
+            image: {kulfi},
+            itemName: "Kulfi",
+            price:"30"        
+        },
+        {
+            id: 20,
+            image: {kulfi},
+            itemName: "Pista Bar 55ml",
+            price:"15"        
+        },
+        {
+            id: 21,
+            image: {red},
+            itemName: "Red Velvet Cone 120ml",
+            price:"35"        
+        },
+        {
+            id: 22,
+            image: {sprite},
+            itemName: "Sprite",
+            price:"40"        
+        },
+        {
+            id: 23,
+            image: {sprite},
+            itemName: "Sprite 600ml",
+            price:"40"        
+        },
+        {
+            id: 24,
+            image: {seven},
+            itemName: "7up 600ml",
+            price:"40"        
+        },
+        {
+            id: 25,
+            image: {badammilk},
+            itemName: "Badam Milk",
+            price:"30"        
+        },
+        {
+            id: 25,
+            image: {cococola},
+            itemName: "Coco-cola",
+            price:"40"        
+        },
+        {
+            id: 26,
+            image: {cooldrinks},
+            itemName: "Cool drinks",
+            price:"40"        
+        },
+        {
+            id: 27,
+            image: {fanta},
+            itemName: "Fanta",
+            price:"40"        
+        },
+
+]
