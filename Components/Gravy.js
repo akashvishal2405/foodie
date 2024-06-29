b79441c931155d80bd9d894b7c7b3a0b0e71ced4
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-29 13:21:42 +0530

    add new files

---
+++ Components/Gravy.js
@@ -0,0 +1,59 @@
+import Chickengravy from '../Image/gravy/chickengravy.jpg';
+import Egg from '../Image/gravy/egg.jpg';
+import Mushroom from '../Image/gravy/mushroom.jpg';
+import Mutton from '../Image/gravy/mutton.jpg';
+import Prawn from '../Image/gravy/prawn.jpg';
+import Fish from '../Image/gravy/fish.jpg';
+
+
+
+export const Gravy = [
+    {
+        id: 1,
+        image: {Chickengravy},
+        itemName: "Chicken Gravy",
+        price:"120"
+    },
+    {
+        id: 2,
+        image: {Egg},
+        itemName: "Egg Gravy",
+        price:"30"
+    },
+    {
+        id: 3,
+        image: {Chickengravy},
+        itemName: "Kaadai 65 Gravy",
+        price:"120"
+    },
+    {
+        id: 4,
+        image: {Mushroom},
+        itemName: "Mushroom Gravy",
+        price:"110"
+    },
+    {
+        id: 5,
+        image: {Mutton},
+        itemName: "Mutton Gravy",
+        price:"180"
+    },
+    {
+        id: 6,
+        image: {Prawn},
+        itemName: "Prawn Manchurian Gravy",
+        price:"120"
+    },
+    {
+        id: 7,
+        image: {Prawn},
+        itemName: "Prawn Gravy",
+        price:"200"
+    },
+    {
+        id: 8,
+        image: {Fish},
+        itemName: "Fish Kuzhambu",
+        price:"150"
+    }
+]
